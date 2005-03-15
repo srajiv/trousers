f96d1a130350331161e92e158bf7518c9451c6cd
kyoder <kyoder>
2005-03-15 02:22:39 +0000

    change an fprintf to LogError

--- src/tspi/gtk/support.c
+++ src/tspi/gtk/support.c
@@ -24,6 +24,8 @@
 
 #include <gtk/gtk.h>
 
+#include "tsplog.h"
+
 #include "support.h"
 
 GtkWidget*
@@ -128,7 +130,7 @@ create_pixbuf                          (const gchar     *filename)
   pixbuf = gdk_pixbuf_new_from_file (pathname, &error);
   if (!pixbuf)
     {
-      fprintf (stderr, "Failed to load pixbuf file: %s: %s\n",
+      LogError ("Failed to load pixbuf file: %s: %s\n",
                pathname, error->message);
       g_error_free (error);
     }
