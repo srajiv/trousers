e14c26ed54f4f552d7e61eda66ca31f9969c48ab
kyoder <kyoder>
2005-06-30 21:14:01 +0000

    added guts of TCSP_GetRegisteredKeyByPublicInfo

--- src/tcsd_api/tcstp.c
+++ src/tcsd_api/tcstp.c
@@ -387,12 +387,57 @@ TCS_OpenContext_RPC_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE * hConte
 }
 
 TSS_RESULT
-TCSP_GetRegisteredKeyByPublicInfo_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE tcsContext, TCPA_ALGORITHM_ID algID,	/*  in */
-						 UINT32 ulPublicInfoLength,	/*  in */
-						 BYTE * rgbPublicInfo,	/*  in */
-						 UINT32 * keySize,
-						 BYTE ** keyBlob) {
-	return TSPERR(TSS_E_NOTIMPL);
+TCSP_GetRegisteredKeyByPublicInfo_TP(struct host_table_entry *hte,
+				     TCS_CONTEXT_HANDLE hContext,
+				     TCPA_ALGORITHM_ID algID,	/* in */
+				     UINT32 ulPublicInfoLength,	/* in */
+				     BYTE * rgbPublicInfo,	/* in */
+				     UINT32 * keySize,		/* out */
+				     BYTE ** keyBlob)		/* out */
+{
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_GETREGISTEREDKEYBYPUBLICINFO;
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 1, &algID, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 2, &ulPublicInfoLength, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 3, rgbPublicInfo, ulPublicInfoLength, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_UINT32, 0, keySize, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+		*keyBlob = (BYTE *) malloc(*keySize);
+		if (*keyBlob == NULL) {
+			LogError("Malloc of %d bytes failed.", *keySize);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto done;
+		}
+		if (getData(TCSD_PACKET_TYPE_PBYTE, 1, *keyBlob, *keySize, hdr)) {
+			free(*keyBlob);
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+	}
+
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
