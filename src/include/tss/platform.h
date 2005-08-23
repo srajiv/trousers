ac804985ec58bd9fc1338de23e2680328e86bfa6
kyoder <kyoder>
2005-08-23 15:32:31 +0000

    changed UNICODE from wchar_t to unsigned short

--- src/include/tss/platform.h
+++ src/include/tss/platform.h
@@ -26,7 +26,7 @@ typedef unsigned char  BYTE;
 typedef signed char    TSS_BOOL;  // Make specific to TSS to avoid potential conflicts
 typedef unsigned short UINT16;
 typedef unsigned int   UINT32;
-typedef wchar_t        UNICODE;
+typedef unsigned short UNICODE;
 typedef void*          PVOID;
 #endif
 
