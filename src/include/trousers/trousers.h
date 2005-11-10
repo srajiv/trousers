13842a72c2e5047572298f977a51ce04a15ed272
kyoder <kyoder>
2005-11-10 21:38:32 +0000

    added new prototypes for identity creation usage

--- src/include/trousers/trousers.h
+++ src/include/trousers/trousers.h
@@ -11,6 +11,8 @@
 #ifndef _TROUSERS_H_
 #define _TROUSERS_H_
 
+#include <openssl/rsa.h>
+
 /*
  * Utility functions offered by trousers for use in your TSS app.
  *
@@ -41,6 +43,10 @@ TSS_RESULT Trspi_UnloadBlob_MigrationKeyAuth(UINT16 *offset, BYTE *blob, TCPA_MI
 TSS_RESULT Trspi_UnloadBlob_PCR_EVENT(UINT16 *offset, BYTE *blob, TSS_PCR_EVENT *event);
 void       Trspi_UnloadBlob_KM_KEYINFO(UINT16 *offset, BYTE *blob, TSS_KM_KEYINFO *info);
 TSS_RESULT Trspi_UnloadBlob_SYMMETRIC_KEY(UINT16 *offset, BYTE *blob, TCPA_SYMMETRIC_KEY *key);
+TSS_RESULT Trspi_UnloadBlob_SYM_CA_ATTESTATION(UINT16 *offset, BYTE *blob, TCPA_SYM_CA_ATTESTATION *sym);
+TSS_RESULT Trspi_UnloadBlob_ASYM_CA_CONTENTS(UINT16 *offset, BYTE *blob, TCPA_ASYM_CA_CONTENTS *asym);
+TSS_RESULT Trspi_UnloadBlob_IDENTITY_REQ(UINT16 *offset, BYTE *blob, TCPA_IDENTITY_REQ *req);
+TSS_RESULT Trspi_UnloadBlob_IDENTITY_PROOF(UINT16 *offset, BYTE *blob, TCPA_IDENTITY_PROOF *proof);
 
 /* Blob loading functions */
 void Trspi_LoadBlob(UINT16 *offset, UINT32 size, BYTE *container, BYTE *object);
@@ -54,6 +60,7 @@ void Trspi_LoadBlob_TCPA_VERSION(UINT16 *offset, BYTE *blob, TCPA_VERSION versio
 void Trspi_LoadBlob_PCR_INFO(UINT16 *offset, BYTE *blob, TCPA_PCR_INFO *pcr);
 void Trspi_LoadBlob_PCR_SELECTION(UINT16 *offset, BYTE *blob, TCPA_PCR_SELECTION *pcr);
 void Trspi_LoadBlob_STORED_DATA(UINT16 *offset, BYTE *blob, TCPA_STORED_DATA *data);
+void Trspi_LoadBlob_PUBKEY(UINT16 *offset, BYTE *blob, TCPA_PUBKEY *pubKey);
 void Trspi_LoadBlob_KEY(UINT16 *offset, BYTE *blob, TCPA_KEY *key);
 void Trspi_LoadBlob_KEY_FLAGS(UINT16 *offset, BYTE *blob, TCPA_KEY_FLAGS *flags);
 void Trspi_LoadBlob_KEY_PARMS(UINT16 *offset, BYTE *blob, TCPA_KEY_PARMS *keyInfo);
@@ -62,9 +69,11 @@ void Trspi_LoadBlob_UUID(UINT16 *offset, BYTE *blob, TSS_UUID uuid);
 void Trspi_LoadBlob_CERTIFY_INFO(UINT16 *offset, BYTE *blob, TCPA_CERTIFY_INFO *certify);
 void Trspi_LoadBlob_STORE_ASYMKEY(UINT16 *offset, BYTE *blob, TCPA_STORE_ASYMKEY *store);
 void Trspi_LoadBlob_PCR_EVENT(UINT16 *offset, BYTE *blob, TSS_PCR_EVENT *event);
-void Trspi_LoadBlob_PRIVKEY_DIGEST(UINT16 * offset, BYTE * blob, TCPA_KEY *key);
+void Trspi_LoadBlob_PRIVKEY_DIGEST(UINT16 *offset, BYTE *blob, TCPA_KEY *key);
 void Trspi_LoadBlob_SYMMETRIC_KEY(UINT16 *offset, BYTE *blob, TCPA_SYMMETRIC_KEY *key);
-
+void Trspi_LoadBlob_SYM_CA_ATTESTATION(UINT16 *offset, BYTE *blob, TCPA_SYM_CA_ATTESTATION *sym);
+void Trspi_LoadBlob_ASYM_CA_CONTENTS(UINT16 *offset, BYTE *blob, TCPA_ASYM_CA_CONTENTS *asym);
+void Trspi_LoadBlob_IDENTITY_REQ(UINT16 *offset, BYTE *blob, TCPA_IDENTITY_REQ *req);
 
 /* Cryptographic Functions */
 
@@ -89,16 +98,28 @@ int Trspi_Verify( UINT32 HashType, BYTE *pHash, UINT32 iHashLength,
 		unsigned char *pModulus, int iKeyLength,
 		BYTE *pSignature, UINT32 sig_len);
 
+int Trspi_RSA_Public_Encrypt(unsigned char *in, unsigned int inlen,
+			     unsigned char *out, unsigned int *outlen,
+			     unsigned char *pubkey, unsigned int pubsize,
+			     unsigned int e, int padding);
 
-/* RSA encrypt @dataToEncryptLen bytes at location @dataToEncrypt using public key
- * @publicKey of size @keysize. This data will be encrypted using PKCS#1.5 padding in
- * the openssl library.  This function is intended to be used with legacy keys. */
-int Trspi_RSA_PKCS15_Encrypt(unsigned char *dataToEncrypt,
-			unsigned int dataToEncryptLen,
-			unsigned char *encryptedData,
-			unsigned int *encryptedDataLen,
-			unsigned char *publicKey,
-			unsigned int keysize);
+#define Trspi_RSA_PKCS15_Encrypt(in,inlen,out,outlen,pubKey,pubSize) \
+        Trspi_RSA_Public_Encrypt(in,inlen,out,outlen,pubKey,pubSize,65537,RSA_PKCS1_PADDING)
+
+#define Trspi_RSA_OAEP_Encrypt(in,inlen,out,outlen,pubKey,pubSize) \
+        Trspi_RSA_Public_Encrypt(in,inlen,out,outlen,pubKey,pubSize,65537, \
+				 RSA_PKCS1_OAEP_PADDING)
+
+#define Trspi_TPM_RSA_OAEP_Encrypt(in,inlen,out,outlen,pubKey,pubSize) \
+        Trspi_RSA_Encrypt(in,inlen,out,outlen,pubKey,pubSize)
+
+/* Symmetric Encryption */
+
+TSS_RESULT Trspi_Encrypt_ECB(UINT16 alg, BYTE *key, BYTE *in, UINT32 in_len,
+			     BYTE *out, UINT32 *out_len);
+TSS_RESULT Trspi_Decrypt_ECB(UINT16 alg, BYTE *key, BYTE *in, UINT32 in_len,
+			     BYTE *out, UINT32 *out_len);
+
 
 /* String Functions */
 
