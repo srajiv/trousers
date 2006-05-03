40123b2f4628024feb2797bc772105096fc2d871
kyoder <kyoder>
2006-05-03 21:21:42 +0000

    added plumbing for migration functions

--- src/tcsd/tcsd_wrap.c
+++ src/tcsd/tcsd_wrap.c
@@ -3840,6 +3840,345 @@ tcs_wrap_ActivateIdentity(struct tcsd_thread_data *data,
 	return TSS_SUCCESS;
 }
 
+TSS_RESULT
+tcs_wrap_CreateMigrationBlob(struct tcsd_thread_data *data,
+			     struct tsp_packet *tsp_data,
+			     struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TCS_KEY_HANDLE parentHandle;
+	TSS_MIGRATE_SCHEME migrationType;
+	UINT32 MigrationKeyAuthSize, encDataSize, randomSize, outDataSize;
+	BYTE *MigrationKeyAuth, *encData, *random, *outData;
+	TPM_AUTH auth1, auth2, *pParentAuth, *pEntityAuth;
+	UINT32 size = sizeof(struct tcsd_packet_hdr), i;
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 1, &parentHandle, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	if (getData(TCSD_PACKET_TYPE_UINT16, 2, &migrationType, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 3, &MigrationKeyAuthSize, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	MigrationKeyAuth = (BYTE *)malloc(MigrationKeyAuthSize);
+	if (MigrationKeyAuth == NULL) {
+		LogError("malloc of %d bytes failed.", MigrationKeyAuthSize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 4, MigrationKeyAuth, MigrationKeyAuthSize, tsp_data)) {
+		free(MigrationKeyAuth);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 5, &encDataSize, 0, tsp_data)) {
+		free(MigrationKeyAuth);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	encData = (BYTE *)malloc(encDataSize);
+	if (encData == NULL) {
+		free(MigrationKeyAuth);
+		LogError("malloc of %d bytes failed.", encDataSize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 6, encData, encDataSize, tsp_data)) {
+		free(MigrationKeyAuth);
+		free(encData);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 7, &auth1, 0, tsp_data)) {
+		free(MigrationKeyAuth);
+		free(encData);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 8, &auth2, 0, tsp_data)) {
+		/* If loading the 2nd auth fails, the first one was entity auth */
+		pParentAuth = NULL;
+		pEntityAuth = &auth1;
+	} else {
+		/* If loading the 2nd auth succeeds, the first one was parent auth */
+		pParentAuth = &auth1;
+		pEntityAuth = &auth2;
+	}
+
+	result = TCSP_CreateMigrationBlob_Internal(hContext, parentHandle, migrationType,
+						   MigrationKeyAuthSize, MigrationKeyAuth,
+						   encDataSize, encData, pParentAuth, pEntityAuth,
+						   &randomSize, &random, &outDataSize, &outData);
+
+	free(MigrationKeyAuth);
+	free(encData);
+	if (result == TSS_SUCCESS) {
+		i = 0;
+		*hdr = calloc(1, size + (2 * sizeof(UINT32)) + (2 * sizeof(TPM_AUTH)) +
+			      randomSize + outDataSize);
+		if (*hdr == NULL) {
+			free(random);
+			free(outData);
+			LogError("malloc of %zd bytes failed.", size + (2 * sizeof(UINT32)) +
+				 (2 * sizeof(TPM_AUTH)) + randomSize + outDataSize);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (pParentAuth) {
+			if (setData(TCSD_PACKET_TYPE_AUTH, i++, pParentAuth, 0, *hdr)) {
+				free(*hdr);
+				free(random);
+				free(outData);
+				return TCSERR(TSS_E_INTERNAL_ERROR);
+			}
+		}
+
+		if (setData(TCSD_PACKET_TYPE_AUTH, i++, pEntityAuth, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, i++, &randomSize, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (randomSize > 0) {
+			if (setData(TCSD_PACKET_TYPE_PBYTE, i++, random, randomSize, *hdr)) {
+				free(*hdr);
+				free(random);
+				free(outData);
+				return TCSERR(TSS_E_INTERNAL_ERROR);
+			}
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, i++, &outDataSize, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, i++, outData, outDataSize, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		free(random);
+		free(outData);
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %d bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_ConvertMigrationBlob(struct tcsd_thread_data *data,
+			      struct tsp_packet *tsp_data,
+			      struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TCS_KEY_HANDLE parentHandle;
+	UINT32 outDataSize, randomSize, inDataSize;
+	BYTE *outData, *random, *inData;
+	TPM_AUTH parentAuth, *pParentAuth;
+	UINT32 size = sizeof(struct tcsd_packet_hdr), i;
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 1, &parentHandle, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 2, &inDataSize, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	inData = (BYTE *)malloc(inDataSize);
+	if (inData == NULL) {
+		LogError("malloc of %d bytes failed.", inDataSize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 3, inData, inDataSize, tsp_data)) {
+		free(inData);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 4, &randomSize, 0, tsp_data)) {
+		free(inData);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	random = (BYTE *)malloc(randomSize);
+	if (random == NULL) {
+		free(inData);
+		LogError("malloc of %d bytes failed.", randomSize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 5, random, randomSize, tsp_data)) {
+		free(inData);
+		free(random);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 6, &parentAuth, 0, tsp_data))
+		pParentAuth = NULL;
+	else
+		pParentAuth = &parentAuth;
+
+
+	result = TCSP_ConvertMigrationBlob_Internal(hContext, parentHandle, inDataSize, inData,
+						    randomSize, random, pParentAuth, &outDataSize,
+						    &outData);
+
+	free(inData);
+	free(random);
+	if (result == TSS_SUCCESS) {
+		i = 0;
+		*hdr = calloc(1, size + sizeof(UINT32) + sizeof(TPM_AUTH) + outDataSize);
+		if (*hdr == NULL) {
+			free(outData);
+			LogError("malloc of %zd bytes failed.", size + sizeof(UINT32) +
+				 sizeof(TPM_AUTH) + outDataSize);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (pParentAuth) {
+			if (setData(TCSD_PACKET_TYPE_AUTH, i++, pParentAuth, 0, *hdr)) {
+				free(*hdr);
+				free(outData);
+				return TCSERR(TSS_E_INTERNAL_ERROR);
+			}
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, i++, &outDataSize, 0, *hdr)) {
+			free(*hdr);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, i++, outData, outDataSize, *hdr)) {
+			free(*hdr);
+			free(outData);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		free(outData);
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %d bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_AuthorizeMigrationKey(struct tcsd_thread_data *data,
+			       struct tsp_packet *tsp_data,
+			       struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TSS_MIGRATE_SCHEME migrateScheme;
+	UINT32 MigrationKeySize, MigrationKeyAuthSize;
+	BYTE *MigrationKey, *MigrationKeyAuth;
+	TPM_AUTH ownerAuth;
+	UINT32 size = sizeof(struct tcsd_packet_hdr);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_UINT16, 1, &migrateScheme, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 2, &MigrationKeySize, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	MigrationKey = (BYTE *)malloc(MigrationKeySize);
+	if (MigrationKey == NULL) {
+		LogError("malloc of %d bytes failed.", MigrationKeySize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 3, MigrationKey, MigrationKeySize, tsp_data)) {
+		free(MigrationKey);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 4, &ownerAuth, 0, tsp_data)) {
+		free(MigrationKey);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	result = TCSP_AuthorizeMigrationKey_Internal(hContext, migrateScheme, MigrationKeySize,
+						     MigrationKey, &ownerAuth,
+						     &MigrationKeyAuthSize, &MigrationKeyAuth);
+
+	free(MigrationKey);
+	if (result == TSS_SUCCESS) {
+		*hdr = calloc(1, size + sizeof(UINT32) + sizeof(TPM_AUTH) + MigrationKeyAuthSize);
+		if (*hdr == NULL) {
+			free(MigrationKeyAuth);
+			LogError("malloc of %zd bytes failed.", size + sizeof(UINT32) +
+				 sizeof(TPM_AUTH) + MigrationKeyAuthSize);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_AUTH, 0, &ownerAuth, 0, *hdr)) {
+			free(*hdr);
+			free(MigrationKeyAuth);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, 1, &MigrationKeyAuthSize, 0, *hdr)) {
+			free(*hdr);
+			free(MigrationKeyAuth);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, 2, MigrationKeyAuth, MigrationKeyAuthSize,
+			    *hdr)) {
+			free(*hdr);
+			free(MigrationKeyAuth);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		free(MigrationKeyAuth);
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %d bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
 /* Dispatch */
 typedef struct tdDispatchTable {
 	TSS_RESULT (*Func) (struct tcsd_thread_data *,
@@ -3887,9 +4226,9 @@ DispatchTable table[TCSD_MAX_NUM_ORDS] = {
 	{tcs_wrap_Seal},
 	{tcs_wrap_UnSeal},
 	{tcs_wrap_UnBind},
-	{tcs_wrap_Error},
-	{tcs_wrap_Error}, /* 40 */
-	{tcs_wrap_Error},
+	{tcs_wrap_CreateMigrationBlob},
+	{tcs_wrap_ConvertMigrationBlob}, /* 40 */
+	{tcs_wrap_AuthorizeMigrationKey},
 	{tcs_wrap_CertifyKey},
 	{tcs_wrap_Sign},
 	{tcs_wrap_GetRandom},
--- src/tcsd_api/tcstp.c
+++ src/tcsd_api/tcstp.c
@@ -2245,7 +2245,7 @@ done:
 TSS_RESULT
 TCSP_CreateMigrationBlob_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hContext,	/* in */
 					TCS_KEY_HANDLE parentHandle,	/* in */
-					TCPA_MIGRATE_SCHEME migrationType,	/* in */
+					TSS_MIGRATE_SCHEME migrationType,	/* in */
 					UINT32 MigrationKeyAuthSize,	/* in */
 					BYTE * MigrationKeyAuth,	/* in */
 					UINT32 encDataSize,	/* in */
@@ -2257,7 +2257,110 @@ TCSP_CreateMigrationBlob_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hCo
 					UINT32 * outDataSize,	/* out */
 					BYTE ** outData	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+	TSS_HCONTEXT tspContext;
+	TPM_AUTH null_auth;
+	UINT32 i;
+
+	if ((tspContext = obj_lookupTspContext(hContext)) == NULL_HCONTEXT)
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+	memset(&null_auth, 0, sizeof(TPM_AUTH));
+
+	data.ordinal = TCSD_ORD_CREATEMIGRATIONBLOB;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	i = 0;
+	if (setData(TCSD_PACKET_TYPE_UINT32, i++, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, i++, &parentHandle, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT16, i++, &migrationType, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, i++, &MigrationKeyAuthSize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, i++, MigrationKeyAuth, MigrationKeyAuthSize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, i++, &encDataSize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, i++, encData, encDataSize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	if (parentAuth) {
+		if (setData(TCSD_PACKET_TYPE_AUTH, i++, parentAuth, 0, &data))
+			return TSPERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (setData(TCSD_PACKET_TYPE_AUTH, i++, entityAuth, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		i = 0;
+		if (parentAuth) {
+			if (getData(TCSD_PACKET_TYPE_AUTH, i++, parentAuth, 0, hdr)) {
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		}
+		if (getData(TCSD_PACKET_TYPE_AUTH, i++, entityAuth, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+
+		if (getData(TCSD_PACKET_TYPE_UINT32, i++, randomSize, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+
+		if (*randomSize > 0) {
+			*random = (BYTE *)calloc_tspi(tspContext, *randomSize);
+			if (*random == NULL) {
+				LogError("malloc of %u bytes failed.", *randomSize);
+				result = TSPERR(TSS_E_OUTOFMEMORY);
+				goto done;
+			}
+			if (getData(TCSD_PACKET_TYPE_PBYTE, i++, *random, *randomSize, hdr)) {
+				free_tspi(tspContext, *random);
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		}
+
+		if (getData(TCSD_PACKET_TYPE_UINT32, i++, outDataSize, 0, hdr)) {
+			if (*randomSize > 0)
+				free_tspi(tspContext, *random);
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+
+		*outData = (BYTE *)calloc_tspi(tspContext, *outDataSize);
+		if (*outData == NULL) {
+			if (*randomSize > 0)
+				free_tspi(tspContext, *random);
+			LogError("malloc of %u bytes failed.", *outDataSize);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto done;
+		}
+		if (getData(TCSD_PACKET_TYPE_PBYTE, i++, *outData, *outDataSize, hdr)) {
+			if (*randomSize > 0)
+				free_tspi(tspContext, *random);
+			free_tspi(tspContext, *outData);
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+	}
+
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
@@ -2265,25 +2368,141 @@ TCSP_ConvertMigrationBlob_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hC
 					 TCS_KEY_HANDLE parentHandle,	/* in */
 					 UINT32 inDataSize,	/* in */
 					 BYTE * inData,	/* in */
+					 UINT32 randomSize,	/* in */
+					 BYTE * random,	/* in */
 					 TPM_AUTH * parentAuth,	/* in, out */
-					 UINT32 randomSize,	/*  should be in */
-					 BYTE * random,	/*  should be in */
 					 UINT32 * outDataSize,	/* out */
 					 BYTE ** outData	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+	UINT32 i;
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_CONVERTMIGRATIONBLOB;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 1, &parentHandle, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 2, &inDataSize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 3, inData, inDataSize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 4, &randomSize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 5, random, randomSize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	if (parentAuth) {
+		if (setData(TCSD_PACKET_TYPE_AUTH, 6, parentAuth, 0, &data))
+			return TSPERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		i = 0;
+		if (parentAuth) {
+			if (getData(TCSD_PACKET_TYPE_AUTH, i++, parentAuth, 0, hdr)) {
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		}
+
+		if (getData(TCSD_PACKET_TYPE_UINT32, i++, outDataSize, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+
+		*outData = (BYTE *)malloc(*outDataSize);
+		if (*outData == NULL) {
+			LogError("malloc of %u bytes failed.", *outDataSize);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto done;
+		}
+		if (getData(TCSD_PACKET_TYPE_PBYTE, i++, *outData, *outDataSize, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+	}
+
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
 TCSP_AuthorizeMigrationKey_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hContext,	/* in */
-					  TCPA_MIGRATE_SCHEME migrateScheme,	/* in */
+					  TSS_MIGRATE_SCHEME migrateScheme,	/* in */
 					  UINT32 MigrationKeySize,	/* in */
 					  BYTE * MigrationKey,	/* in */
 					  TPM_AUTH * ownerAuth,	/* in, out */
 					  UINT32 * MigrationKeyAuthSize,	/* out */
 					  BYTE ** MigrationKeyAuth	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+	TSS_HCONTEXT tspContext;
+
+	if ((tspContext = obj_lookupTspContext(hContext)) == NULL_HCONTEXT)
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_AUTHORIZEMIGRATIONKEY;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT16, 1, &migrateScheme, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 2, &MigrationKeySize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 3, MigrationKey, MigrationKeySize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_AUTH, 4, ownerAuth, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_AUTH, 0, ownerAuth, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+		if (getData(TCSD_PACKET_TYPE_UINT32, 1, MigrationKeyAuthSize, 0, hdr)) {
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+
+		*MigrationKeyAuth = (BYTE *)calloc_tspi(tspContext, *MigrationKeyAuthSize);
+		if (*MigrationKeyAuth == NULL) {
+			LogError("malloc of %u bytes failed.", *MigrationKeyAuthSize);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto done;
+		}
+		if (getData(TCSD_PACKET_TYPE_PBYTE, 2, *MigrationKeyAuth, *MigrationKeyAuthSize,
+			    hdr)) {
+			free(*MigrationKeyAuth);
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+			goto done;
+		}
+	}
+
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
