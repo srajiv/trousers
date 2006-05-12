9ae89d6a46e21f6f20da0da214e03f0b1ad476fa
kyoder <kyoder>
2006-05-12 13:55:16 +0000

    added build variables to the get cap variables for TSP and TCSD versions

--- src/include/capabilities.h
+++ src/include/capabilities.h
@@ -4,34 +4,33 @@
  *
  * trousers - An open source TCG Software Stack
  *
- * (C) Copyright International Business Machines Corp. 2004
+ * (C) Copyright International Business Machines Corp. 2004, 2006
  *
  */
 
 #ifndef _CAPABILITIES_H_
 #define _CAPABILITIES_H_
 
-/**********************************************
-	This header has all of the software capabilities
-		that are returned either via
-		Tspi_Context_GetCapability or
-		TCS_GetCapability.
-***********************************************/
+/* This header has all of the software capabilities that are returned either via
+ * Tspi_Context_GetCapability or TCS_GetCapability.
+ */
 
 /* TSP */
-#define INTERNAL_CAP_TSP_VERSION		{ 0x01, 0x00, 0x00, 0x01 }
+#define INTERNAL_CAP_TSP_VERSION		{ TSS_VER_MAJOR, TSS_VER_MINOR, \
+						  TSS_REV_MAJOR, TSS_REV_MINOR }
 
 #define INTERNAL_CAP_TSP_ALG_RSA		TRUE
 #define INTERNAL_CAP_TSP_ALG_SHA		TRUE
 #define INTERNAL_CAP_TSP_ALG_3DES		FALSE
 #define INTERNAL_CAP_TSP_ALG_DES		FALSE
-#define INTERNAL_CAP_TSP_ALG_HMAC		FALSE
-#define INTERNAL_CAP_TSP_ALG_AES		FALSE
+#define INTERNAL_CAP_TSP_ALG_HMAC		TRUE
+#define INTERNAL_CAP_TSP_ALG_AES		TRUE
 
 #define INTERNAL_CAP_TSP_PERSSTORAGE		TRUE
 
 /* TCS */
-#define INTERNAL_CAP_TCS_VERSION		{ 0x01, 0x00, 0x00, 0x01 }
+#define INTERNAL_CAP_TCS_VERSION		{ TSS_VER_MAJOR, TSS_VER_MINOR, \
+						  TSS_REV_MAJOR, TSS_REV_MINOR }
 
 #define INTERNAL_CAP_TCS_ALG_RSA		FALSE
 #define INTERNAL_CAP_TCS_ALG_AES		FALSE
@@ -43,7 +42,7 @@
 #define INTERNAL_CAP_TCS_PERSSTORAGE		TRUE
 
 #define INTERNAL_CAP_TCS_CACHING_KEYCACHE	TRUE
-#define INTERNAL_CAP_TCS_CACHING_AUTHCACHE	FALSE
+#define INTERNAL_CAP_TCS_CACHING_AUTHCACHE	TRUE
 
 #define INTERNAL_CAP_TCS_MANUFACTURER	{ 'I', 'B', 'M', ' ' };
 
