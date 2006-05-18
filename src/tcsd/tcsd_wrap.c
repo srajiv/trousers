56154057f539a5a8e320224cb5e233b5dec5adea
kyoder <kyoder>
2006-05-18 18:37:26 +0000

    added implementation of maintenance functions

--- src/tcsd/tcsd_wrap.c
+++ src/tcsd/tcsd_wrap.c
@@ -4142,7 +4142,7 @@ tcs_wrap_AuthorizeMigrationKey(struct tcsd_thread_data *data,
 		*hdr = calloc(1, size + sizeof(UINT32) + sizeof(TPM_AUTH) + MigrationKeyAuthSize);
 		if (*hdr == NULL) {
 			free(MigrationKeyAuth);
-			LogError("malloc of %zd bytes failed.", size + sizeof(UINT32) +
+			LogError("malloc of %zu bytes failed.", size + sizeof(UINT32) +
 				 sizeof(TPM_AUTH) + MigrationKeyAuthSize);
 			return TCSERR(TSS_E_OUTOFMEMORY);
 		}
@@ -4202,7 +4202,7 @@ tcs_wrap_KillMaintenanceFeature(struct tcsd_thread_data *data,
 	if (result == TSS_SUCCESS) {
 		*hdr = calloc(1, size + sizeof(TPM_AUTH));
 		if (*hdr == NULL) {
-			LogError("malloc of %zd bytes failed.", size + sizeof(TPM_AUTH));
+			LogError("malloc of %zu bytes failed.", size + sizeof(TPM_AUTH));
 			return TCSERR(TSS_E_OUTOFMEMORY);
 		}
 
@@ -4213,7 +4213,279 @@ tcs_wrap_KillMaintenanceFeature(struct tcsd_thread_data *data,
 	} else {
 		*hdr = calloc(1, size);
 		if (*hdr == NULL) {
-			LogError("malloc of %d bytes failed.", size);
+			LogError("malloc of %u bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_CreateMaintenanceArchive(struct tcsd_thread_data *data,
+				  struct tsp_packet *tsp_data,
+				  struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TPM_AUTH ownerAuth;
+	TSS_BOOL generateRandom;
+	UINT32 randomSize, archiveSize;
+	BYTE *random, *archive;
+	UINT32 size = sizeof(struct tcsd_packet_hdr);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_BOOL, 1, &generateRandom, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 2, &ownerAuth, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	result = TCSP_CreateMaintenanceArchive_Internal(hContext, generateRandom, &ownerAuth,
+							&randomSize, &random, &archiveSize,
+							&archive);
+
+	if (result == TSS_SUCCESS) {
+		*hdr = calloc(1, size + sizeof(TPM_AUTH) + (2 * sizeof(UINT32)) +
+			      randomSize + archiveSize);
+		if (*hdr == NULL) {
+			free(random);
+			free(archive);
+			LogError("malloc of %zu bytes failed.", size + sizeof(TPM_AUTH) +
+				 (2 * sizeof(UINT32)) + randomSize + archiveSize);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_AUTH, 0, &ownerAuth, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(archive);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, 1, &randomSize, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(archive);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, 2, random, randomSize, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(archive);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, 3, &archiveSize, 0, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(archive);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, 4, archive, archiveSize, *hdr)) {
+			free(*hdr);
+			free(random);
+			free(archive);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		free(random);
+		free(archive);
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %u bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_LoadMaintenanceArchive(struct tcsd_thread_data *data,
+				struct tsp_packet *tsp_data,
+				struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TPM_AUTH ownerAuth;
+	UINT32 dataInSize, dataOutSize;
+	BYTE *dataIn, *dataOut;
+	UINT32 size = sizeof(struct tcsd_packet_hdr);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 1, &dataInSize, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	dataIn = (BYTE *)malloc(dataInSize);
+	if (dataIn == NULL) {
+		LogError("malloc of %d bytes failed.", dataInSize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 2, dataIn, dataInSize, tsp_data)) {
+		free(dataIn);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_AUTH, 3, &ownerAuth, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	result = TCSP_LoadMaintenanceArchive_Internal(hContext, dataInSize, dataIn, &ownerAuth,
+							&dataOutSize, &dataOut);
+	free(dataIn);
+
+	if (result == TSS_SUCCESS) {
+		*hdr = calloc(1, size + sizeof(TPM_AUTH) + sizeof(UINT32) + dataOutSize);
+		if (*hdr == NULL) {
+			free(dataOut);
+			LogError("malloc of %zu bytes failed.", size + sizeof(TPM_AUTH) +
+				 sizeof(UINT32) + dataOutSize);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_AUTH, 0, &ownerAuth, 0, *hdr)) {
+			free(*hdr);
+			free(dataOut);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_UINT32, 1, &dataOutSize, 0, *hdr)) {
+			free(*hdr);
+			free(dataOut);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+		if (setData(TCSD_PACKET_TYPE_PBYTE, 2, dataOut, dataOutSize, *hdr)) {
+			free(*hdr);
+			free(dataOut);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+
+		free(dataOut);
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %u bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_LoadManuMaintPub(struct tcsd_thread_data *data,
+			  struct tsp_packet *tsp_data,
+			  struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	UINT32 pubKeySize;
+	BYTE *pubKey;
+	TCPA_NONCE antiReplay;
+	TCPA_DIGEST checksum;
+	UINT32 size = sizeof(struct tcsd_packet_hdr);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_NONCE, 1, &antiReplay, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 2, &pubKeySize, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	pubKey = (BYTE *)malloc(pubKeySize);
+	if (pubKey == NULL) {
+		LogError("malloc of %d bytes failed.", pubKeySize);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	if (getData(TCSD_PACKET_TYPE_PBYTE, 3, pubKey, pubKeySize, tsp_data)) {
+		free(pubKey);
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	result = TCSP_LoadManuMaintPub_Internal(hContext, antiReplay, pubKeySize, pubKey,
+						&checksum);
+	free(pubKey);
+
+	if (result == TSS_SUCCESS) {
+		*hdr = calloc(1, size + sizeof(TCPA_DIGEST));
+		if (*hdr == NULL) {
+			LogError("malloc of %zu bytes failed.", size + sizeof(TCPA_DIGEST));
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_DIGEST, 0, &checksum, 0, *hdr)) {
+			free(*hdr);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %u bytes failed.", size);
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+		(*hdr)->packet_size = size;
+	}
+	(*hdr)->result = result;
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+tcs_wrap_ReadManuMaintPub(struct tcsd_thread_data *data,
+			  struct tsp_packet *tsp_data,
+			  struct tcsd_packet_hdr **hdr)
+{
+	TCS_CONTEXT_HANDLE hContext;
+	TSS_RESULT result;
+	TCPA_NONCE antiReplay;
+	TCPA_DIGEST checksum;
+	UINT32 size = sizeof(struct tcsd_packet_hdr);
+
+	if (getData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	LogDebugFn("thread %x context %x", (UINT32)pthread_self(), hContext);
+
+	if (getData(TCSD_PACKET_TYPE_NONCE, 1, &antiReplay, 0, tsp_data))
+		return TCSERR(TSS_E_INTERNAL_ERROR);
+
+	result = TCSP_ReadManuMaintPub_Internal(hContext, antiReplay, &checksum);
+
+	if (result == TSS_SUCCESS) {
+		*hdr = calloc(1, size + sizeof(TCPA_DIGEST));
+		if (*hdr == NULL) {
+			LogError("malloc of %zu bytes failed.", size + sizeof(TCPA_DIGEST));
+			return TCSERR(TSS_E_OUTOFMEMORY);
+		}
+
+		if (setData(TCSD_PACKET_TYPE_DIGEST, 0, &checksum, 0, *hdr)) {
+			free(*hdr);
+			return TCSERR(TSS_E_INTERNAL_ERROR);
+		}
+	} else {
+		*hdr = calloc(1, size);
+		if (*hdr == NULL) {
+			LogError("malloc of %u bytes failed.", size);
 			return TCSERR(TSS_E_OUTOFMEMORY);
 		}
 		(*hdr)->packet_size = size;
@@ -4300,11 +4572,11 @@ DispatchTable table[TCSD_MAX_NUM_ORDS] = {
 	{tcs_wrap_PhysicalPresence},
 	{tcs_wrap_Error},
 	{tcs_wrap_Error},
-	{tcs_wrap_Error},
-	{tcs_wrap_Error}, /* 70 */
+	{tcs_wrap_CreateMaintenanceArchive},
+	{tcs_wrap_LoadMaintenanceArchive}, /* 70 */
 	{tcs_wrap_KillMaintenanceFeature},
-	{tcs_wrap_Error},
-	{tcs_wrap_Error}
+	{tcs_wrap_LoadManuMaintPub},
+	{tcs_wrap_ReadManuMaintPub}
 };
 
 int
--- src/tcsd_api/tcstp.c
+++ src/tcsd_api/tcstp.c
@@ -3480,20 +3480,138 @@ TCSP_CreateMaintenanceArchive_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDL
 					     UINT32 * archiveSize,	/* out */
 					     BYTE ** archive	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+	TSS_HCONTEXT tspContext;
 
+	if ((tspContext = obj_lookupTspContext(hContext)) == NULL_HCONTEXT)
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_CREATEMAINTENANCEARCHIVE;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_BOOL, 1, &generateRandom, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_AUTH, 2, ownerAuth, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_AUTH, 0, ownerAuth, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+		if (getData(TCSD_PACKET_TYPE_UINT32, 1, randomSize, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+
+		if (*randomSize > 0) {
+			*random = calloc_tspi(tspContext, *randomSize);
+			if (*random == NULL) {
+				LogError("malloc of %u bytes failed.", *randomSize);
+				result = TSPERR(TSS_E_OUTOFMEMORY);
+				goto done;
+			}
+
+			if (getData(TCSD_PACKET_TYPE_PBYTE, 2, *random, *randomSize, hdr)) {
+				free_tspi(tspContext, *random);
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		} else {
+			*random = NULL;
+		}
+
+		/* Assume all elements are in the list, even when *randomSize == 0. */
+		if (getData(TCSD_PACKET_TYPE_UINT32, 3, archiveSize, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+
+		if (*archiveSize > 0) {
+			*archive = calloc_tspi(tspContext, *archiveSize);
+			if (*archive == NULL) {
+				LogError("malloc of %u bytes failed.", *archiveSize);
+				result = TSPERR(TSS_E_OUTOFMEMORY);
+				goto done;
+			}
+
+			if (getData(TCSD_PACKET_TYPE_PBYTE, 4, *archive, *archiveSize, hdr)) {
+				free_tspi(tspContext, *random);
+				free_tspi(tspContext, *archive);
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		} else {
+			*archive = NULL;
+		}
+	}
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
 TCSP_LoadMaintenanceArchive_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hContext,	/* in */
 					   UINT32 dataInSize,	/* in */
 					   BYTE * dataIn,	/* in */
+					   TPM_AUTH * ownerAuth,	/* in, out */
 					   UINT32 * dataOutSize,	/* out */
-					   BYTE ** dataOut,	/* out */
-					   TPM_AUTH * ownerAuth	/* in, out */
+					   BYTE ** dataOut	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
 
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_LOADMAINTENANCEARCHIVE;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 1, &dataInSize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 2, &dataIn, dataInSize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_AUTH, 3, ownerAuth, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_AUTH, 0, ownerAuth, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+		if (getData(TCSD_PACKET_TYPE_UINT32, 1, dataOutSize, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+
+		if (*dataOutSize > 0) {
+			*dataOut = malloc(*dataOutSize);
+			if (*dataOut == NULL) {
+				LogError("malloc of %u bytes failed.", *dataOutSize);
+				result = TSPERR(TSS_E_OUTOFMEMORY);
+				goto done;
+			}
+
+			if (getData(TCSD_PACKET_TYPE_PBYTE, 2, *dataOut, *dataOutSize, hdr)) {
+				free(*dataOut);
+				result = TSPERR(TSS_E_INTERNAL_ERROR);
+				goto done;
+			}
+		} else {
+			*dataOut = NULL;
+		}
+	}
+done:
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
@@ -3511,7 +3629,7 @@ TCSP_KillMaintenanceFeature_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE 
 
 	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
 		return TSPERR(TSS_E_INTERNAL_ERROR);
-	if (setData(TCSD_PACKET_TYPE_AUTH, 1, &ownerAuth, 0, &data))
+	if (setData(TCSD_PACKET_TYPE_AUTH, 1, ownerAuth, 0, &data))
 		return TSPERR(TSS_E_INTERNAL_ERROR);
 
 	result = sendTCSDPacket(hte, 0, &data, &hdr);
@@ -3526,7 +3644,6 @@ TCSP_KillMaintenanceFeature_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE 
 
 	free(hdr);
 	return result;
-
 }
 
 TSS_RESULT
@@ -3536,8 +3653,36 @@ TCSP_LoadManuMaintPub_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hConte
 				     BYTE * PubKey,	/* in */
 				     TCPA_DIGEST * checksum	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
 
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_LOADMANUFACTURERMAINTENANCEPUB;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_NONCE, 1, &antiReplay, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_UINT32, 2, &PubKeySize, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_PBYTE, 3, &PubKey, PubKeySize, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_DIGEST, 0, checksum, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	free(hdr);
+	return result;
 }
 
 TSS_RESULT
@@ -3545,5 +3690,30 @@ TCSP_ReadManuMaintPub_TP(struct host_table_entry *hte, TCS_CONTEXT_HANDLE hConte
 				     TCPA_NONCE antiReplay,	/* in */
 				     TCPA_DIGEST * checksum	/* out */
     ) {
-	return TSPERR(TSS_E_NOTIMPL);
+	TSS_RESULT result;
+	struct tsp_packet data;
+	struct tcsd_packet_hdr *hdr;
+
+	memset(&data, 0, sizeof(struct tsp_packet));
+
+	data.ordinal = TCSD_ORD_READMANUFACTURERMAINTENANCEPUB;
+	LogDebugFn("TCS Context: 0x%x", hContext);
+
+	if (setData(TCSD_PACKET_TYPE_UINT32, 0, &hContext, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+	if (setData(TCSD_PACKET_TYPE_NONCE, 1, &antiReplay, 0, &data))
+		return TSPERR(TSS_E_INTERNAL_ERROR);
+
+	result = sendTCSDPacket(hte, 0, &data, &hdr);
+
+	if (result == TSS_SUCCESS)
+		result = hdr->result;
+
+	if (result == TSS_SUCCESS) {
+		if (getData(TCSD_PACKET_TYPE_DIGEST, 0, checksum, 0, hdr))
+			result = TSPERR(TSS_E_INTERNAL_ERROR);
+	}
+
+	free(hdr);
+	return result;
 }
