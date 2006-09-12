8788ae3c100435b8aa79043a747f8778338a2f27
kyoder <kyoder>
2006-09-12 16:41:13 +0000

    added support for prepending a random IV to the cipher text when none is explicitly passed into the symmetric encryption routines

--- src/trspi/crypto/openssl/crypto.c
+++ src/trspi/crypto/openssl/crypto.c
@@ -417,7 +417,14 @@ Trspi_SymEncrypt(UINT16 alg, BYTE mode, BYTE *key, BYTE *iv, BYTE *in, UINT32 in
 	TSS_RESULT result = TSS_SUCCESS;
 	EVP_CIPHER_CTX ctx;
 	EVP_CIPHER *cipher;
+	BYTE *def_iv = NULL, *outiv_ptr;
 	UINT32 tmp;
+	int iv_len, outiv_len;
+
+	if (*out_len > INT_MAX)
+		outiv_len = INT_MAX;
+	else
+		outiv_len = *(int *)out_len;
 
 	/* TPM 1.1 had no defines for symmetric encryption modes, must use CBC */
 	switch (mode) {
@@ -450,32 +457,55 @@ Trspi_SymEncrypt(UINT16 alg, BYTE mode, BYTE *key, BYTE *iv, BYTE *in, UINT32 in
 
 	EVP_CIPHER_CTX_init(&ctx);
 
-	if (!EVP_EncryptInit(&ctx, (const EVP_CIPHER *)cipher, key, iv)) {
+	/* If the iv passed in is NULL, create a new random iv and prepend it to the ciphertext */
+	if (iv == NULL) {
+		iv_len = EVP_CIPHER_iv_length(cipher);
+		def_iv = malloc(iv_len);
+		if (def_iv == NULL) {
+			LogError("malloc of %d bytes failed.", iv_len);
+			return TSPERR(TSS_E_OUTOFMEMORY);
+		}
+		RAND_bytes(def_iv, iv_len);
+
+		memcpy(out, def_iv, iv_len);
+		outiv_ptr = &out[iv_len];
+		outiv_len -= iv_len;
+	} else {
+		def_iv = iv;
+		outiv_ptr = out;
+	}
+
+	if (!EVP_EncryptInit(&ctx, (const EVP_CIPHER *)cipher, key, def_iv)) {
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		DEBUG_print_openssl_errors();
 		goto done;
 	}
 
-	if (*out_len < in_len + EVP_CIPHER_CTX_block_size(&ctx) - 1) {
+	if ((UINT32)outiv_len < in_len + (EVP_CIPHER_CTX_block_size(&ctx) * 2) - 1) {
 		LogDebug("Not enough space to do symmetric encryption");
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		goto done;
 	}
 
-	if (!EVP_EncryptUpdate(&ctx, out, (int *)out_len, in, in_len)) {
+	if (!EVP_EncryptUpdate(&ctx, outiv_ptr, &outiv_len, in, in_len)) {
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		DEBUG_print_openssl_errors();
 		goto done;
 	}
 
-	if (!EVP_EncryptFinal(&ctx, out + *out_len, (int *)&tmp)) {
+	if (!EVP_EncryptFinal(&ctx, outiv_ptr + outiv_len, (int *)&tmp)) {
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		DEBUG_print_openssl_errors();
 		goto done;
 	}
 
-	*out_len += tmp;
+	outiv_len += tmp;
+	*out_len = outiv_len;
 done:
+	if (def_iv != iv) {
+		*out_len += iv_len;
+		free(def_iv);
+	}
 	EVP_CIPHER_CTX_cleanup(&ctx);
 	return result;
 }
@@ -487,7 +517,9 @@ Trspi_SymDecrypt(UINT16 alg, BYTE mode, BYTE *key, BYTE *iv, BYTE *in, UINT32 in
 	TSS_RESULT result = TSS_SUCCESS;
 	EVP_CIPHER_CTX ctx;
 	EVP_CIPHER *cipher;
+	BYTE *def_iv = NULL, *iniv_ptr;
 	UINT32 tmp;
+	int iv_len, iniv_len;
 
 	/* TPM 1.1 had no defines for symmetric encryption modes, must use CBC */
 	switch (mode) {
@@ -520,13 +552,31 @@ Trspi_SymDecrypt(UINT16 alg, BYTE mode, BYTE *key, BYTE *iv, BYTE *in, UINT32 in
 
 	EVP_CIPHER_CTX_init(&ctx);
 
-	if (!EVP_DecryptInit(&ctx, cipher, key, iv)) {
+	/* If the iv is NULL, assume that its prepended to the ciphertext */
+	if (iv == NULL) {
+		iv_len = EVP_CIPHER_iv_length(cipher);
+		def_iv = malloc(iv_len);
+		if (def_iv == NULL) {
+			LogError("malloc of %d bytes failed.", iv_len);
+			return TSPERR(TSS_E_OUTOFMEMORY);
+		}
+		RAND_bytes(def_iv, iv_len);
+
+		memcpy(def_iv, in, iv_len);
+		iniv_ptr = &in[iv_len];
+		iniv_len = in_len - iv_len;
+	} else {
+		def_iv = iv;
+		iniv_ptr = in;
+	}
+
+	if (!EVP_DecryptInit(&ctx, cipher, key, def_iv)) {
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		DEBUG_print_openssl_errors();
 		goto done;
 	}
 
-	if (!EVP_DecryptUpdate(&ctx, out, (int *)out_len, in, in_len)) {
+	if (!EVP_DecryptUpdate(&ctx, out, out_len, iniv_ptr, iniv_len)) {
 		result = TSPERR(TSS_E_INTERNAL_ERROR);
 		DEBUG_print_openssl_errors();
 		goto done;
@@ -540,6 +590,8 @@ Trspi_SymDecrypt(UINT16 alg, BYTE mode, BYTE *key, BYTE *iv, BYTE *in, UINT32 in
 
 	*out_len += tmp;
 done:
+	if (def_iv != iv)
+		free(def_iv);
 	EVP_CIPHER_CTX_cleanup(&ctx);
 	return result;
 }
