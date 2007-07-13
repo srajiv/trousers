fbd8290042b733a903608e1a27f1469ceec508aa
kyoder <kyoder>
2007-07-13 21:13:31 +0000

    replaced blob manipulations with calls to tpm_rqu_build and tpm_rsp_parse

--- src/tcs/tcsi_aik.c
+++ src/tcs/tcsi_aik.c
@@ -48,7 +48,6 @@ TCSP_MakeIdentity_Internal(TCS_CONTEXT_HANDLE hContext,			/* in  */
 	UINT64 offset;
 	UINT32 paramSize;
 	TSS_RESULT result;
-	TCPA_KEY idKeyContainer;
 	BYTE txBlob[TSS_TPM_TXBLOB_SIZE];
 
 	if ((result = ctx_verify_context(hContext)))
@@ -66,53 +65,20 @@ TCSP_MakeIdentity_Internal(TCS_CONTEXT_HANDLE hContext,			/* in  */
 		goto done;
 
 	offset = 0;
-
-	offset = 10;
-	/*LoadBlob( &offset, idKeyInfoSize, txBlob, idKeyInfo);  */
-	LoadBlob(&offset, TCPA_ENCAUTH_SIZE, txBlob, identityAuth.authdata);
-	/*LoadBlob_UINT32( &offset, 20, txBlob, "label size"); */
-	LoadBlob(&offset, 20, txBlob, IDLabel_PrivCAHash.digest);
-	LoadBlob(&offset, idKeyInfoSize, txBlob, idKeyInfo);
-	if (pSrkAuth != NULL) {
-		LoadBlob_Auth(&offset, txBlob, pSrkAuth);
-		LoadBlob_Auth(&offset, txBlob, pOwnerAuth);
-		LoadBlob_Header(TPM_TAG_RQU_AUTH2_COMMAND, offset, TPM_ORD_MakeIdentity, txBlob);
-	} else {
-		LoadBlob_Auth(&offset, txBlob, pOwnerAuth);
-		LoadBlob_Header(TPM_TAG_RQU_AUTH1_COMMAND, offset, TPM_ORD_MakeIdentity, txBlob);
-	}
+	if ((result = tpm_rqu_build(TPM_ORD_MakeIdentity, &offset, txBlob, identityAuth.authdata,
+				    IDLabel_PrivCAHash.digest, idKeyInfoSize, idKeyInfo, pSrkAuth,
+				    pOwnerAuth)))
+		goto done;
 
 	if ((result = req_mgr_submit_req(txBlob)))
 		goto done;
 
-	offset = 10;
 	result = UnloadBlob_Header(txBlob, &paramSize);
-
 	if (!result) {
-		/* Call UnloadBlob_KEY to set the size of the key in &offset */
-		if ((result = UnloadBlob_KEY(&offset, txBlob, &idKeyContainer)))
+		if ((result = tpm_rsp_parse(TPM_ORD_MakeIdentity, txBlob, paramSize, idKeySize,
+					    idKey, pcIdentityBindingSize, prgbIdentityBinding,
+					    pSrkAuth, pOwnerAuth)))
 			goto done;
-
-		destroy_key_refs(&idKeyContainer);
-		*idKeySize = offset - 10;
-		*idKey = calloc(1, *idKeySize);
-		if (*idKey == NULL) {
-			LogError("malloc of %d bytes failed.", *idKeySize);
-			result = TCSERR(TSS_E_OUTOFMEMORY);
-			goto done;
-		}
-		memcpy(*idKey, &txBlob[10], *idKeySize);
-
-		UnloadBlob_UINT32(&offset, pcIdentityBindingSize, txBlob);
-		*prgbIdentityBinding = calloc(1, *pcIdentityBindingSize);
-		if (*prgbIdentityBinding == NULL) {
-			free(*idKey);
-			*idKeySize = 0;
-			LogError("malloc of %d bytes failed.", *pcIdentityBindingSize);
-			result = TCSERR(TSS_E_OUTOFMEMORY);
-			goto done;
-		}
-		UnloadBlob(&offset, *pcIdentityBindingSize, txBlob, *prgbIdentityBinding);
 
 		/* If an error occurs, these will return NULL */
 		get_credential(TSS_TCS_CREDENTIAL_PLATFORMCERT, pcPlatformCredentialSize,
@@ -121,10 +87,6 @@ TCSP_MakeIdentity_Internal(TCS_CONTEXT_HANDLE hContext,			/* in  */
 			       prgbConformanceCredential);
 		get_credential(TSS_TCS_CREDENTIAL_EKCERT, pcEndorsementCredentialSize,
 			       prgbEndorsementCredential);
-
-		if (pSrkAuth != NULL)
-			UnloadBlob_Auth(&offset, txBlob, pSrkAuth);
-		UnloadBlob_Auth(&offset, txBlob, pOwnerAuth);
 	}
 	LogResult("Make Identity", result);
 done:
@@ -142,8 +104,7 @@ TCSP_ActivateTPMIdentity_Internal(TCS_CONTEXT_HANDLE hContext,	/* in */
 				  UINT32 * SymmetricKeySize,	/* out */
 				  BYTE ** SymmetricKey)	/* out */
 {
-	UINT64 offset, authOffset;
-	TCPA_SYMMETRIC_KEY symKey;
+	UINT64 offset;
 	TSS_RESULT result;
 	UINT32 paramSize;
 	UINT32 keySlot;
@@ -164,57 +125,19 @@ TCSP_ActivateTPMIdentity_Internal(TCS_CONTEXT_HANDLE hContext,	/* in */
 	if ((result = ensureKeyIsLoaded(hContext, idKey, &keySlot)))
 		goto done;
 
-	offset = 10;
-	LoadBlob_UINT32(&offset, keySlot, txBlob);
-	LoadBlob_UINT32(&offset, blobSize, txBlob);
-	LoadBlob(&offset, blobSize, txBlob, blob);
-	if (idKeyAuth != NULL) {
-		LoadBlob_Auth(&offset, txBlob, idKeyAuth);
-		LoadBlob_Auth(&offset, txBlob, ownerAuth);
-		LoadBlob_Header(TPM_TAG_RQU_AUTH2_COMMAND,
-				offset,
-				TPM_ORD_ActivateIdentity, txBlob);
-	} else {
-		LoadBlob_Auth(&offset, txBlob, ownerAuth);
-		LoadBlob_Header(TPM_TAG_RQU_AUTH1_COMMAND,
-				offset,
-				TPM_ORD_ActivateIdentity, txBlob);
-	}
+	offset = 0;
+	if ((result = tpm_rqu_build(TPM_ORD_ActivateIdentity, &offset, txBlob, keySlot, blobSize,
+				    blob, idKeyAuth, ownerAuth)))
+		goto done;
 
 	if ((result = req_mgr_submit_req(txBlob)))
 		goto done;
 
 	result = UnloadBlob_Header(txBlob, &paramSize);
-
 	if (!result) {
-		/* We don't know what kind of key the symmetric key is, or how big it is.
-		 * So, call UnloadBlob_SYMMETRIC_KEY to parse through the returned data
-		 * and create the expanded TCPA_SYMMETRIC_KEY structure.  Then, serialize
-		 * that data to pass back to the TSP. */
-		offset = 10;
-		if ((result = UnloadBlob_SYMMETRIC_KEY(&offset, txBlob, &symKey)))
+		if ((result = tpm_rsp_parse(TPM_ORD_ActivateIdentity, txBlob, paramSize,
+					    SymmetricKeySize, SymmetricKey, idKeyAuth, ownerAuth)))
 			goto done;
-
-		/* After parsing through the symmetric key, offset will point to the auth
-		 * structure(s) */
-		authOffset = offset;
-
-		*SymmetricKey = calloc(1, offset - 10);
-		if (*SymmetricKey == NULL) {
-			free(symKey.data);
-			LogError("malloc of %" PRIu64 " bytes failed.", offset - 10);
-			result = TCSERR(TSS_E_OUTOFMEMORY);
-			goto done;
-		}
-		*SymmetricKeySize = offset - 10;
-		offset = 0;
-		LoadBlob_SYMMETRIC_KEY(&offset, *SymmetricKey, &symKey);
-		free(symKey.data);
-
-		if (idKeyAuth != NULL) {
-			UnloadBlob_Auth(&authOffset, txBlob, idKeyAuth);
-		}
-		UnloadBlob_Auth(&authOffset, txBlob, ownerAuth);
 	}
 
 done:
