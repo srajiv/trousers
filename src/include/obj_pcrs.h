3a229fed0ccfbbc78ed451c14fe9c8a395378a73
kyoder <kyoder>
2007-08-13 20:25:50 +0000

    updated with new pcr apis; list freeing code

--- src/include/obj_pcrs.h
+++ src/include/obj_pcrs.h
@@ -4,7 +4,7 @@
  *
  * trousers - An open source TCG Software Stack
  *
- * (C) Copyright International Business Machines Corp. 2004-2006
+ * (C) Copyright International Business Machines Corp. 2004-2007
  *
  */
 
@@ -15,7 +15,7 @@
 
 /* structures */
 struct tr_pcrs_obj {
-	int type;
+	UINT32 type;
 	union {
 		TPM_PCR_INFO info11;
 		TPM_PCR_INFO_SHORT infoshort;
@@ -25,19 +25,23 @@ struct tr_pcrs_obj {
 };
 
 /* obj_pcrs.c */
+void       pcrs_free(void *data);
 TSS_BOOL   obj_is_pcrs(TSS_HOBJECT);
 TSS_RESULT obj_pcrs_get_tsp_context(TSS_HPCRS, TSS_HCONTEXT *);
 TSS_RESULT obj_pcrs_add(TSS_HCONTEXT, UINT32, TSS_HOBJECT *);
 TSS_RESULT obj_pcrs_remove(TSS_HOBJECT, TSS_HCONTEXT);
+TSS_RESULT obj_pcrs_get_type(TSS_HPCRS, UINT32 *);
 TSS_RESULT obj_pcrs_select_index(TSS_HPCRS, UINT32);
 TSS_RESULT obj_pcrs_select_index_ex(TSS_HPCRS, UINT32, UINT32);
 TSS_RESULT obj_pcrs_get_value(TSS_HPCRS, UINT32, UINT32 *, BYTE **);
 TSS_RESULT obj_pcrs_set_value(TSS_HPCRS, UINT32, UINT32, BYTE *);
 TSS_RESULT obj_pcrs_set_values(TSS_HPCRS hPcrs, TCPA_PCR_COMPOSITE *);
 TSS_RESULT obj_pcrs_get_selection(TSS_HPCRS, UINT32 *, BYTE *);
-TSS_RESULT obj_pcrs_get_digest_at_creation(TSS_HPCRS, TCPA_PCRVALUE *);
 TSS_RESULT obj_pcrs_get_digest_at_release(TSS_HPCRS, UINT32 *, BYTE **);
+TSS_RESULT obj_pcrs_create_info_type(TSS_HPCRS, UINT32, UINT32 *, BYTE **);
 TSS_RESULT obj_pcrs_create_info(TSS_HPCRS, UINT32 *, BYTE **);
+TSS_RESULT obj_pcrs_create_info_long(TSS_HPCRS, UINT32 *, BYTE **);
+TSS_RESULT obj_pcrs_create_info_short(TSS_HPCRS, UINT32 *, BYTE **);
 TSS_RESULT obj_pcrs_get_locality(TSS_HPCRS, UINT32 *);
 TSS_RESULT obj_pcrs_set_locality(TSS_HPCRS, UINT32);
 
@@ -45,7 +49,7 @@ TSS_RESULT obj_pcrs_set_locality(TSS_HPCRS, UINT32);
 #define PCRS_LIST_DECLARE_EXTERN	extern struct obj_list pcrs_list
 #define PCRS_LIST_INIT()		list_init(&pcrs_list)
 #define PCRS_LIST_CONNECT(a,b)		obj_connectContext_list(&pcrs_list, a, b)
-#define PCRS_LIST_CLOSE(a)		obj_list_close(&pcrs_list, a)
+#define PCRS_LIST_CLOSE(a)		obj_list_close(&pcrs_list, &pcrs_free, a)
 
 #else
 
