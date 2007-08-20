2122af2519ec1dc0b378f3759b5aaed672d3c2b3
kyoder <kyoder>
2007-08-20 20:45:17 +0000

    added tsplog.h include; transport support

--- src/tspi/tsp_audit.c
+++ src/tspi/tsp_audit.c
@@ -17,6 +17,7 @@
 #include "trousers/trousers.h"
 #include "trousers_types.h"
 #include "spi_utils.h"
+#include "tsplog.h"
 #include "obj.h"
 
 
@@ -79,3 +80,180 @@ audit_set_ordinal_audit_status(TSS_HTPM hTpm,
 
 	return obj_policy_validate_auth_oiap(hPolicy, &digest, &ownerAuth);
 }
+
+#ifdef TSS_BUILD_TRANSPORT
+TSS_RESULT
+Transport_SetOrdinalAuditStatus(TSS_HCONTEXT tspContext,        /* in */
+				TPM_AUTH *ownerAuth,          /* in/out */
+				UINT32 ulOrdinal,             /* in */
+				TSS_BOOL bAuditState) /* in */
+{
+        TSS_RESULT result;
+        UINT32 handlesLen = 0;
+        UINT64 offset;
+        BYTE data[sizeof(UINT32) + sizeof(TSS_BOOL)];
+
+
+	if ((result = obj_context_transport_init(tspContext)))
+		return result;
+
+	LogDebugFn("Executing in a transport session");
+
+	offset = 0;
+	Trspi_LoadBlob_UINT32(&offset, ulOrdinal, data);
+	Trspi_LoadBlob_BOOL(&offset, bAuditState, data);
+
+	result = obj_context_transport_execute(tspContext, TPM_ORD_SetOrdinalAuditStatus,
+					       sizeof(data), data, NULL, &handlesLen, NULL,
+					       ownerAuth, NULL, NULL, NULL);
+	return result;
+}
+
+TSS_RESULT
+Transport_GetAuditDigest(TSS_HCONTEXT tspContext,       /* in */
+			 UINT32 startOrdinal,         /* in */
+			 TPM_DIGEST *auditDigest,             /* out */
+			 UINT32 *counterValueSize,            /* out */
+			 BYTE **counterValue,         /* out */
+			 TSS_BOOL *more,                      /* out */
+			 UINT32 *ordSize,                     /* out */
+			 UINT32 **ordList)                    /* out */
+{
+        TSS_RESULT result;
+        UINT32 handlesLen = 0, decLen, i;
+        BYTE *dec = NULL;
+        UINT64 offset;
+        BYTE data[sizeof(UINT32)];
+
+
+	if ((result = obj_context_transport_init(tspContext)))
+		return result;
+
+	LogDebugFn("Executing in a transport session");
+
+	offset = 0;
+	Trspi_LoadBlob_UINT32(&offset, startOrdinal, data);
+
+	if ((result = obj_context_transport_execute(tspContext, TPM_ORD_GetAuditDigestSigned,
+						    sizeof(data), data, NULL, &handlesLen, NULL,
+						    NULL, NULL, &decLen, &dec)))
+		return result;
+
+	offset = 0;
+	Trspi_UnloadBlob_COUNTER_VALUE(&offset, dec, NULL);
+
+	*counterValueSize = (UINT32)offset;
+	if ((*counterValue = malloc(*counterValueSize)) == NULL) {
+		free(dec);
+		LogError("malloc of %u bytes failed", *counterValueSize);
+		*counterValueSize = 0;
+		return TSPERR(TSS_E_OUTOFMEMORY);
+	}
+	offset = 0;
+	Trspi_UnloadBlob(&offset, *counterValueSize, dec, *counterValue);
+
+	Trspi_UnloadBlob_DIGEST(&offset, dec, auditDigest);
+	Trspi_UnloadBlob_BOOL(&offset, more, dec);
+
+	Trspi_UnloadBlob_UINT32(&offset, ordSize, dec);
+
+	if ((*ordList = malloc(*ordSize)) == NULL) {
+		free(dec);
+		free(*counterValue);
+		*counterValue = NULL;
+		*counterValueSize = 0;
+		LogError("malloc of %u bytes failed", *ordSize);
+		*counterValueSize = 0;
+		return TSPERR(TSS_E_OUTOFMEMORY);
+	}
+
+	for (i = 0; i < *ordSize / sizeof(UINT32); i++)
+		Trspi_UnloadBlob_UINT32(&offset, (UINT32 *)&ordList[i * sizeof(UINT32)], dec);
+
+	return TSS_SUCCESS;
+}
+
+TSS_RESULT
+Transport_GetAuditDigestSigned(TSS_HCONTEXT tspContext,       /* in */
+			       TCS_KEY_HANDLE keyHandle,      /* in */
+			       TSS_BOOL closeAudit,           /* in */
+			       TPM_NONCE *antiReplay,         /* in */
+			       TPM_AUTH *privAuth,            /* in/out */
+			       UINT32 *counterValueSize,      /* out */
+			       BYTE **counterValue,           /* out */
+			       TPM_DIGEST *auditDigest,       /* out */
+			       TPM_DIGEST *ordinalDigest,     /* out */
+			       UINT32 *sigSize,               /* out */
+			       BYTE **sig)                    /* out */
+{
+        TSS_RESULT result;
+        UINT32 handlesLen, decLen;
+        TCS_HANDLE *handles;
+        BYTE *dec = NULL;
+        TPM_DIGEST pubKeyHash;
+        Trspi_HashCtx hashCtx;
+        UINT64 offset;
+        BYTE data[sizeof(TSS_BOOL) + sizeof(TPM_NONCE)];
+
+
+	if ((result = obj_context_transport_init(tspContext)))
+		return result;
+
+	LogDebugFn("Executing in a transport session");
+
+	if ((result = obj_tcskey_get_pubkeyhash(keyHandle, pubKeyHash.digest)))
+		return result;
+
+	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
+	result |= Trspi_Hash_DIGEST(&hashCtx, pubKeyHash.digest);
+	if ((result |= Trspi_HashFinal(&hashCtx, pubKeyHash.digest)))
+		return result;
+
+	handlesLen = 1;
+	if ((handles = malloc(sizeof(TCS_HANDLE))) == NULL) {
+		LogError("malloc of %zd bytes failed", sizeof(TCS_HANDLE));
+		return TSPERR(TSS_E_OUTOFMEMORY);
+	}
+
+	*handles = keyHandle;
+
+	offset = 0;
+	Trspi_LoadBlob_BOOL(&offset, closeAudit, data);
+	Trspi_LoadBlob_NONCE(&offset, data, antiReplay);
+
+	if ((result = obj_context_transport_execute(tspContext, TPM_ORD_GetAuditDigestSigned,
+						    sizeof(data), data, &pubKeyHash, &handlesLen,
+						    &handles, privAuth, NULL, &decLen, &dec)))
+		return result;
+
+	offset = 0;
+	Trspi_UnloadBlob_COUNTER_VALUE(&offset, dec, NULL);
+
+	*counterValueSize = (UINT32)offset;
+	if ((*counterValue = malloc(*counterValueSize)) == NULL) {
+		free(dec);
+		LogError("malloc of %u bytes failed", *counterValueSize);
+		*counterValueSize = 0;
+		return TSPERR(TSS_E_OUTOFMEMORY);
+	}
+	offset = 0;
+	Trspi_UnloadBlob(&offset, *counterValueSize, dec, *counterValue);
+
+	Trspi_UnloadBlob_DIGEST(&offset, dec, auditDigest);
+	Trspi_UnloadBlob_DIGEST(&offset, dec, ordinalDigest);
+
+	Trspi_UnloadBlob_UINT32(&offset, sigSize, dec);
+
+	if ((*sig = malloc(*sigSize)) == NULL) {
+		free(dec);
+		free(*counterValue);
+		*counterValue = NULL;
+		*counterValueSize = 0;
+		LogError("malloc of %u bytes failed", *sigSize);
+		*counterValueSize = 0;
+		return TSPERR(TSS_E_OUTOFMEMORY);
+	}
+
+	return TSS_SUCCESS;
+}
+#endif
