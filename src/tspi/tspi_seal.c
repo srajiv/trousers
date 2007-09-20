389b0586d54d4e649c9d8a89898f9d1d04c79d77
kyoder <kyoder>
2007-09-20 21:33:39 +0000

    corrected callback support and updated auth session handling

--- src/tspi/tspi_seal.c
+++ src/tspi/tspi_seal.c
@@ -20,6 +20,7 @@
 #include "capabilities.h"
 #include "tsplog.h"
 #include "obj.h"
+#include "authsess.h"
 
 
 TSS_RESULT
@@ -29,25 +30,20 @@ Tspi_Data_Seal(TSS_HENCDATA hEncData,	/* in */
 	       BYTE * rgbDataToSeal,	/* in */
 	       TSS_HPCRS hPcrComposite)	/* in */
 {
-	BYTE sharedSecret[20];
-	TPM_AUTH auth;
-	TCPA_ENCAUTH encAuthUsage;
-	TCPA_ENCAUTH encAuthMig;
-	TCPA_DIGEST digest;
-	TCPA_RESULT result;
+	TPM_DIGEST digest;
+	TSS_RESULT result;
 	TSS_HPOLICY hPolicy, hEncPolicy;
 	BYTE *encData = NULL;
 	UINT32 encDataSize;
-	UINT32 pcrInfoType;
+	UINT32 pcrInfoType = TSS_PCRS_STRUCT_DEFAULT;
 	UINT32 pcrDataSize;
 	BYTE *pcrData;
 	TCS_KEY_HANDLE tcsKeyHandle;
-	TCPA_NONCE nonceOddOSAP;
-	TCPA_NONCE nonceEvenOSAP;
 	TSS_HCONTEXT tspContext;
 	Trspi_HashCtx hashCtx;
-	UINT32 sealOrdinal;
+	UINT32 sealOrdinal = TPM_ORD_Seal;
 	BYTE *sealData;
+	struct authsess *xsap = NULL;
 #ifdef TSS_BUILD_SEALX
 	UINT32 protectMode;
 #endif
@@ -83,35 +79,42 @@ Tspi_Data_Seal(TSS_HENCDATA hEncData,	/* in */
 		pcrInfoType = TSS_PCRS_STRUCT_INFO_LONG;
 	} else
 		return TSPERR(TSS_E_INTERNAL_ERROR);
-#else
-	sealOrdinal = TPM_ORD_Seal;
-	pcrInfoType = 0;
 #endif
 
 	/* If PCR's are of interest */
 	pcrDataSize = 0;
 	if (hPcrComposite) {
-		if ((result = obj_pcrs_create_info_type(hPcrComposite, pcrInfoType,
-				&pcrDataSize, &pcrData)))
+		if ((result = obj_pcrs_create_info_type(hPcrComposite, &pcrInfoType, &pcrDataSize,
+							&pcrData)))
 			return result;
 	}
 
-	if ((result = secret_PerformXOR_OSAP(hPolicy, hEncPolicy, hEncPolicy, hEncKey,
-					     TCPA_ET_KEYHANDLE, tcsKeyHandle, &encAuthUsage,
-					     &encAuthMig, sharedSecret, &auth, &nonceEvenOSAP)))
+	if ((result = authsess_xsap_init(tspContext, hEncKey, hEncData, TSS_AUTH_POLICY_REQUIRED,
+					 sealOrdinal, TPM_ET_KEYHANDLE, &xsap)))
 		return result;
-	nonceOddOSAP = auth.NonceOdd;
 
 #ifdef TSS_BUILD_SEALX
 	if (sealOrdinal == TPM_ORD_Seal)
 		sealData = rgbDataToSeal;
 	else {
-		/* Mask the input data before sending it */
-		if ((result = obj_policy_do_sealx_mask(hEncPolicy, hEncKey, hEncData, &auth,
-						       &nonceEvenOSAP, &nonceOddOSAP,
-						       ulDataLength, rgbDataToSeal,
-						       &sealData)))
-			return result;
+		sealData = NULL;
+		if ((sealData = (BYTE *)calloc(1, ulDataLength)) == NULL) {
+			LogError("malloc of %u bytes failed", ulDataLength);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto error;
+		}
+
+		if ((result =
+		     ((TSS_RESULT (*)(PVOID, TSS_HKEY, TSS_HENCDATA, TSS_ALGORITHM_ID,
+		     UINT32, BYTE *, BYTE *, BYTE *, BYTE *, UINT32, BYTE *,
+		     BYTE *))xsap->cb_sealx.callback)(xsap->cb_sealx.appData, hEncKey, hEncData,
+						      xsap->cb_sealx.alg, sizeof(TPM_NONCE),
+						      xsap->auth.NonceEven.nonce,
+						      xsap->auth.NonceOdd.nonce,
+						      xsap->nonceEvenOSAP.nonce,
+						      xsap->nonceOddOSAP.nonce, ulDataLength,
+						      rgbDataToSeal, sealData)))
+			goto error;
 	}
 #else
 	sealData = rgbDataToSeal;
@@ -119,7 +122,7 @@ Tspi_Data_Seal(TSS_HENCDATA hEncData,	/* in */
 
 	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
 	result |= Trspi_Hash_UINT32(&hashCtx, sealOrdinal);
-	result |= Trspi_HashUpdate(&hashCtx, TCPA_SHA1_160_HASH_LEN, encAuthUsage.authdata);
+	result |= Trspi_Hash_ENCAUTH(&hashCtx, xsap->encAuthUse.authdata);
 	result |= Trspi_Hash_UINT32(&hashCtx, pcrDataSize);
 	result |= Trspi_HashUpdate(&hashCtx, pcrDataSize, pcrData);
 	result |= Trspi_Hash_UINT32(&hashCtx, ulDataLength);
@@ -130,67 +133,58 @@ Tspi_Data_Seal(TSS_HENCDATA hEncData,	/* in */
 		return result;
 	}
 
-	if ((result = secret_PerformAuth_OSAP(hEncKey, TPM_ORD_Seal, hPolicy, hEncPolicy,
-					      hEncPolicy, sharedSecret, &auth, digest.digest,
-					      &nonceEvenOSAP))) {
-		if (sealData != rgbDataToSeal)
-			free(sealData);
-		return result;
-	}
+	if ((result = authsess_xsap_hmac(xsap, &digest)))
+		goto error;
 
 #ifdef TSS_BUILD_SEALX
 	if (sealOrdinal == TPM_ORD_Seal) {
-		if ((result = TCS_API(tspContext)->Seal(tspContext, tcsKeyHandle, &encAuthUsage,
+		if ((result = TCS_API(tspContext)->Seal(tspContext, tcsKeyHandle, &xsap->encAuthUse,
 							pcrDataSize, pcrData, ulDataLength,
-							sealData, &auth, &encDataSize, &encData)))
+							sealData, xsap->pAuth, &encDataSize,
+							&encData)))
 			return result;
 	} else if (sealOrdinal == TPM_ORD_Sealx) {
-		result = TCS_API(tspContext)->Sealx(tspContext, tcsKeyHandle, &encAuthUsage,
+		result = TCS_API(tspContext)->Sealx(tspContext, tcsKeyHandle, &xsap->encAuthUse,
 						    pcrDataSize, pcrData, ulDataLength, sealData,
-						    &auth, &encDataSize, &encData);
+						    xsap->pAuth, &encDataSize, &encData);
 		free(sealData);
 
 		if (result != TSS_SUCCESS)
-			return result;
-	} else
-		return TSPERR(TSS_E_INTERNAL_ERROR);
+			goto error;
+	} else {
+		result = TSPERR(TSS_E_INTERNAL_ERROR);
+		goto error;
+	}
 #else
 	if ((result = TCS_API(tspContext)->Seal(tspContext, tcsKeyHandle, &encAuthUsage,
 						pcrDataSize, pcrData, ulDataLength, sealData, &auth,
 						&encDataSize, &encData)))
-		return result;
+		goto error;
 #endif
 
 	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
 	result |= Trspi_Hash_UINT32(&hashCtx, result);
 	result |= Trspi_Hash_UINT32(&hashCtx, sealOrdinal);
 	result |= Trspi_HashUpdate(&hashCtx, encDataSize, encData);
-	if ((result |= Trspi_HashFinal(&hashCtx, digest.digest))) {
-		free(encData);
-		return result;
-	}
+	if ((result |= Trspi_HashFinal(&hashCtx, digest.digest)))
+		goto error;
 
-	if ((result = secret_ValidateAuth_OSAP(hEncKey, TPM_ORD_Seal, hPolicy, hEncPolicy,
-					       hEncPolicy, sharedSecret, &auth, digest.digest,
-					       &nonceEvenOSAP))) {
-		free(encData);
-		return result;
-	}
+	if ((result = authsess_xsap_verify(xsap, &digest)))
+		goto error;
 
 	/* Need to set the object with the blob and the pcr's */
-	if ((result = obj_encdata_set_data(hEncData, encDataSize, encData))) {
-		free(encData);
-		return result;
-	}
+	if ((result = obj_encdata_set_data(hEncData, encDataSize, encData)))
+		goto error;
 
+	if (pcrDataSize)
+		result = obj_encdata_set_pcr_info(hEncData, pcrInfoType, pcrData);
+
+error:
+	free(xsap);
 	free(encData);
+	free(pcrData);
 
-	if (pcrDataSize) {
-		if ((result = obj_encdata_set_pcr_info(hEncData, pcrData)))
-			return result;
-	}
-
-	return TSS_SUCCESS;
+	return result;
 }
 
 TSS_RESULT
@@ -199,15 +193,19 @@ Tspi_Data_Unseal(TSS_HENCDATA hEncData,		/* in */
 		 UINT32 * pulUnsealedDataLength,/* out */
 		 BYTE ** prgbUnsealedData)	/* out */
 {
-	TPM_AUTH privAuth, privAuth2;
-	TCPA_DIGEST digest;
-	TCPA_RESULT result;
+	UINT64 offset;
+	TPM_AUTH privAuth2;
+	TPM_DIGEST digest;
+	TPM_NONCE authLastNonceEven;
+	TSS_RESULT result;
 	TSS_HPOLICY hPolicy, hEncPolicy;
 	TCS_KEY_HANDLE tcsKeyHandle;
         TSS_HCONTEXT tspContext;
-	UINT32 ulDataLen;
-	BYTE *data;
+	UINT32 ulDataLen, unSealedDataLen;
+	BYTE *data = NULL, *unSealedData = NULL, *maskedData;
+	UINT16 mask;
 	Trspi_HashCtx hashCtx;
+	struct authsess *xsap = NULL;
 
 	if (pulUnsealedDataLength == NULL || prgbUnsealedData == NULL)
 		return TSPERR(TSS_E_BAD_PARAMETER);
@@ -226,52 +224,106 @@ Tspi_Data_Unseal(TSS_HENCDATA hEncData,		/* in */
 		       TSPERR(TSS_E_ENC_NO_DATA) :
 		       result;
 
+	offset = 0;
+	Trspi_UnloadBlob_UINT16(&offset, &mask, data);
+	if (mask == TPM_TAG_STORED_DATA12) {
+		/* The second UINT16 in a TPM_STORED_DATA12 is the entity type. If its non-zero
+		 * then we must unmask the unsealed data after it returns from the TCS */
+		Trspi_UnloadBlob_UINT16(&offset, &mask, data);
+	}
+
 	if ((result = obj_rsakey_get_tcs_handle(hKey, &tcsKeyHandle)))
-		return result;
+		goto error;
+
+	if ((result = authsess_xsap_init(tspContext, hKey, hEncData, TSS_AUTH_POLICY_REQUIRED,
+					 TPM_ORD_Unseal, TPM_ET_KEYHANDLE, &xsap)))
+		goto error;
 
 	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
 	result |= Trspi_Hash_UINT32(&hashCtx, TPM_ORD_Unseal);
 	result |= Trspi_HashUpdate(&hashCtx, ulDataLen, data);
 	if ((result |= Trspi_HashFinal(&hashCtx, digest.digest)))
-		return result;
+		goto error;
 
-	if ((result = secret_PerformAuth_OIAP(hKey, TPM_ORD_Unseal, hPolicy, FALSE, &digest,
-					      &privAuth)))
-		return result;
+	if ((result = authsess_xsap_hmac(xsap, &digest)))
+		goto error;
 
 	if ((result = secret_PerformAuth_OIAP(hEncData, TPM_ORD_Unseal, hEncPolicy, FALSE, &digest,
 					      &privAuth2)))
-		return result;
+		goto error;
 
-	if ((result = TCS_API(tspContext)->Unseal(tspContext, tcsKeyHandle, ulDataLen, data,
-						  &privAuth, &privAuth2, pulUnsealedDataLength,
-						  prgbUnsealedData)))
-		return result;
-
-	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
-	result |= Trspi_Hash_UINT32(&hashCtx, result);
-	result |= Trspi_Hash_UINT32(&hashCtx, TPM_ORD_Unseal);
-	result |= Trspi_Hash_UINT32(&hashCtx, *pulUnsealedDataLength);
-	result |= Trspi_HashUpdate(&hashCtx, *pulUnsealedDataLength, *prgbUnsealedData);
-	if ((result |= Trspi_HashFinal(&hashCtx, digest.digest))) {
-		free(*prgbUnsealedData);
-		return result;
+	if (mask) {
+		/* save off last nonce even to pass to sealx callback */
+		   memcpy(authLastNonceEven.nonce, xsap->auth.NonceEven.nonce, sizeof(TPM_NONCE));
 	}
 
-	if ((result = obj_policy_validate_auth_oiap(hPolicy, &digest, &privAuth))) {
-		free(*prgbUnsealedData);
-		return result;
+	if ((result = TCS_API(tspContext)->Unseal(tspContext, tcsKeyHandle, ulDataLen, data,
+						  xsap->pAuth, &privAuth2, &unSealedDataLen,
+						  &unSealedData)))
+		goto error;
+
+	result = Trspi_HashInit(&hashCtx, TSS_HASH_SHA1);
+	result |= Trspi_Hash_UINT32(&hashCtx, TSS_SUCCESS);
+	result |= Trspi_Hash_UINT32(&hashCtx, TPM_ORD_Unseal);
+	result |= Trspi_Hash_UINT32(&hashCtx, unSealedDataLen);
+	result |= Trspi_HashUpdate(&hashCtx, unSealedDataLen, unSealedData);
+	if ((result |= Trspi_HashFinal(&hashCtx, digest.digest))) {
+		free(unSealedData);
+		goto error;
+	}
+
+	if ((result = authsess_xsap_verify(xsap, &digest))) {
+		free(unSealedData);
+		goto error;
 	}
 
 	if ((result = obj_policy_validate_auth_oiap(hEncPolicy, &digest, &privAuth2))) {
-		free(*prgbUnsealedData);
-		return result;
+		free(unSealedData);
+		goto error;
 	}
 
-	if ((result = add_mem_entry(tspContext, *prgbUnsealedData))) {
-		free(*prgbUnsealedData);
-		return result;
+	/* If the data is masked, use the callback set up in authsess_xsap_init */
+	if (mask) {
+		maskedData = unSealedData;
+
+		if ((unSealedData = calloc_tspi(tspContext, unSealedDataLen)) == NULL) {
+			free(maskedData);
+			LogError("malloc of %u bytes failed", unSealedDataLen);
+			result = TSPERR(TSS_E_OUTOFMEMORY);
+			goto error;
+		}
+
+		/* XXX pass in out saved-off authLastNonceEven. This conflicts with the
+		 * description of the rgbNonceEven parameter in the spec, but without it, its not
+		 * possible to compute the MGF1 key */
+		if ((result =
+		     ((TSS_RESULT (*)(PVOID, TSS_HKEY, TSS_HENCDATA, TSS_ALGORITHM_ID,
+		       UINT32, BYTE *, BYTE *, BYTE *, BYTE *, UINT32, BYTE *,
+		       BYTE *))xsap->cb_sealx.callback)(xsap->cb_sealx.appData, hKey, hEncData,
+							 xsap->cb_sealx.alg, sizeof(TPM_NONCE),
+							 authLastNonceEven.nonce,
+							 xsap->auth.NonceOdd.nonce,
+							 xsap->nonceEvenOSAP.nonce,
+							 xsap->nonceOddOSAP.nonce,
+							 unSealedDataLen, maskedData,
+							 unSealedData))) {
+			free(maskedData);
+			goto error;
+		}
+
+		free(maskedData);
+	} else {
+		if ((result = add_mem_entry(tspContext, unSealedData)))
+			goto error;
 	}
 
-	return TSS_SUCCESS;
+	*pulUnsealedDataLength = unSealedDataLen;
+	*prgbUnsealedData = unSealedData;
+
+error:
+	free(xsap);
+	if (data)
+		free_tspi(tspContext, data);
+
+	return result;
 }
