21b315b9a0e45b2718081f924948843b8473cb16
kyoder <kyoder>
2007-11-19 19:29:34 +0000

    added header file and LogTSPERR function

--- src/tspi/log.c
+++ src/tspi/log.c
@@ -11,6 +11,8 @@
 
 #include <stdio.h>
 #include <string.h>
+
+#include "trousers/tss.h"
 #include "tsplog.h"
 
 #ifdef TSS_DEBUG
@@ -46,6 +48,15 @@ LogBlobData(char *szDescriptor, unsigned long sizeOfBlob, unsigned char *blob)
 		snprintf(&temp[(i%16)*3], 4, "%.2X ", blob[i]);
 	}
 	fprintf(stdout, "%s\n", temp);
+}
+
+TSS_RESULT
+LogTSPERR(TSS_RESULT result, char *file, int line)
+{
+	if (getenv("TSS_DEBUG_OFF") == NULL)
+		fprintf(stderr, "%s %s %s:%d: 0x%x\n", "LOG_RETERR", APPID, file, line, result);
+
+	return (result | TSS_LAYER_TSP);
 }
 
 #endif
